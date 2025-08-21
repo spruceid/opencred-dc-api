3276175ee8138559fabee6a08d1f3e5c45fa9d7d
Ryan Tate <ryan.tate@spruceid.com>
2025-08-21 15:27:35 -0700

    use wasm supported openid4vp
    Signed-off-by: Ryan Tate <ryan.tate@spruceid.com>

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,15 +1,18 @@
+use openid4vp::dc_api::DCAPIRequest;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub async fn initiate_dc_api(
     // config: JsValue,
-    // request: JsValue,
+    request: JsValue,
     // session: JsValue,
     user_agent: String,
-) -> Result<String, JsValue> {
+) -> Result<JsValue, JsValue> {
+    let request: DCAPIRequest = serde_wasm_bindgen::from_value(request)?;
 
-    // TOOD: Add logging support.
+    // echo the request back
 
-    Ok(user_agent)
+    let value = serde_wasm_bindgen::to_value(&request)?;
 
+    Ok(value)
 }
